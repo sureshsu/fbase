2ac48f3a3dd5f3b2322ecf998698c18e4b5ac2a2
Suresh Sundararaman <suresh.sundararaman@nagra.com>
2022-12-02 12:51:39 +0530

    undo changes

--- fcm-test/public/app.js
+++ fcm-test/public/app.js
@@ -1,4 +1,3 @@
-import { register } from 'register-service-worker'
 
 const firebaseConfig = {
           
@@ -22,32 +21,6 @@ const firebaseConfig = {
 // init_fb_analytics();
 
 // isSupported();
-
-
-register('/service-worker.js', {
-  registrationOptions: { scope: './' },
-  ready (registration) {
-    console.log('Service worker is active.')
-  },
-  registered (registration) {
-    console.log('Service worker has been registered.')
-  },
-  cached (registration) {
-    console.log('Content has been cached for offline use.')
-  },
-  updatefound (registration) {
-    console.log('New content is downloading.')
-  },
-  updated (registration) {
-    console.log('New content is available; please refresh.')
-  },
-  offline () {
-    console.log('No internet connection found. App is running in offline mode.')
-  },
-  error (error) {
-    console.error('Error during service worker registration:', error)
-  }
-})
 
 function SubscribeUsers() {
 Notification.requestPermission().then(Permission => {
