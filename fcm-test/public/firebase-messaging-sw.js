44fd0c7f4d99334a381034ee8a7cff98c1a2f84e
Suresh Sundararaman <suresh.sundararaman@nagra.com>
2022-12-02 13:40:56 +0530

    path chngs

--- fcm-test/public/firebase-messaging-sw.js
+++ fcm-test/public/firebase-messaging-sw.js
@@ -7,7 +7,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compa
 
 
  if ('serviceWorker' in navigator) {
-     navigator.serviceWorker.register('../firebase-messaging-sw.js')
+     navigator.serviceWorker.register('https://sureshsu.github.io/fbase/fcm-test/public/firebase-messaging-sw.js')
        .then(function(registration) {
          console.log('Registration successful, scope is:', registration.scope);
        }).catch(function(err) {
