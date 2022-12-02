c83f57f87ef30f2c0f3b2a48f0370ce119c593e2
Suresh Sundararaman <suresh.sundararaman@nagra.com>
2022-12-02 13:16:48 +0530

    new4

--- fcm-test/public/app.js
+++ fcm-test/public/app.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   };
       
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  // firebase.initializeApp(firebaseConfig);
 //   console.log(app);
   // const analytics = getAnalytics(app);
   // console.log(analytics);
