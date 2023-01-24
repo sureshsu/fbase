b7e30e36c9c7a98c96361868b787b7b01675b7a5
sureshsu <suresh.pgsem2008@gmail.com>
2023-01-24 18:46:30 +0530

    Update firebase-messaging-sw.js

--- fcm-test/qk/firebase-messaging-sw.js
+++ fcm-test/qk/firebase-messaging-sw.js
@@ -35,12 +35,13 @@
  // messages.
  const messaging = firebase.messaging();
  if ('serviceWorker' in navigator) {
-  window.navigator.serviceWorker.register('/firebase-messaging-sw.js',  { scope: '/' })
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    }).catch(function(err) {
-     console.log('Service worker registration failed, error:', err);
-    });
+   navigator.serviceWorker.register('../firebase-messaging-sw.js')
+       .then(function(registration) {
+         console.log('Registration successful, scope is:', registration.scope);
+       }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+       });
+    }
  }
 
 // If you would like to customize notifications that are received in the
