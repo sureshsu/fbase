cd8fc33598da7379b69a88add0218540b66c7f58
sureshsu <suresh.pgsem2008@gmail.com>
2023-01-24 18:44:33 +0530

    dot

--- fcm-test/qk/firebase-messaging-sw.js
+++ fcm-test/qk/firebase-messaging-sw.js
@@ -56,7 +56,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = 'Background Message Title';
   const notificationOptions = {
     body: 'Background Message body.',
-    icon: './firebase-logo.png'
+    icon: '/firebase-logo.png'
   };
 
   self.registration.showNotification(notificationTitle,
